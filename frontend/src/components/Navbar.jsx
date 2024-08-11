7c6e36cbf2336dd31d3ab8d04a1d4c0c18868373
Gagan G Kurup <gagangkurup10@gmail.com>
2024-08-11 12:45:19 +0530

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <AppBar position="static" sx={{ backgroundColor: '#9c27b0' }}>
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          EmployeeApp
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/add">Add</Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
